49983a95f35e52bd054391769a5365885d45235d
Nessaj <ksnessaj@hotmail.com>
2023-02-17 18:24:48 +0800

    indev20230217

--- assets/scripts/datagen.py
+++ assets/scripts/datagen.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import json
 
+def dumpJson(filename, gendict):
+    with open(f"../dataparsed/{filename}.json", "w", encoding='utf8') as fp:
+        json.dump(gendict, fp, ensure_ascii=False, indent=4)
+
 def genCharStat():
-    df = pd.read_csv("../datamine/Chars.csv")
+    df = pd.read_csv(f"../datamine/Chars.csv")
     chardict = {}
     for i in range(3, 35):
         chardict[df.iat[i,0]] = {}
@@ -10,10 +14,10 @@ def genCharStat():
             if pd.notna(df.iat[1,j]):
                 chardict[df.iat[i,0]][df.iat[1,j]] = df.iat[i,j]
         chardict[df.iat[i,0]]["Internal Level"] = df.iat[i,14]
+    dumpJson("Chars", chardict)
     return chardict
+
 
 if __name__ == '__main__':
     chardict = genCharStat()
-    with open("../characters/charstats.json", "w", encoding='utf8') as fp:
-        json.dump(chardict, fp, ensure_ascii=False, indent=4)
 
